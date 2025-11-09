ceb10ff17f9c969b2d7c35cffeae7d8eace81bf6
giftlion <netanelac770@gmail.com>
2025-11-09 14:40:36 +0200

    first mod func

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 
 // Hello returns a greeting for the named person.
-func printHello(name string) string {
+func PrintHello(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
